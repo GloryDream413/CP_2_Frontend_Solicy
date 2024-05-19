c5b05034b402c872fab946e77bb3546adca36d00
rothschild <rothschild@gmail.com>
2024-05-19 16:04:20 +0900

    Implement About Solicy panel

--- src/components/home/feature.tsx
+++ src/components/home/feature.tsx
@@ -5,6 +5,7 @@ import Grid from '@mui/material/Grid'
 import { styled } from '@mui/material/styles'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 import CircularProgress from '@mui/material/CircularProgress'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
 import { data } from './feature.data'
@@ -37,107 +38,12 @@ const BorderLinearProgress = styled(LinearProgress, {
 
 const HomeFeature: FC = () => {
   return (
-    <Box id="feature" sx={{ py: { xs: 10, md: 14 }, backgroundColor: 'background.paper' }}>
+    <Box id="feature" sx={{ py: { xs: 10, md: 14 }, backgroundColor: 'custom_background.estimate' }}>
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12} md={5}>
             <Box sx={{ position: 'relative' }}>
-              <Image src="/images/home-feature.png" width={650} height={678} quality={97} alt="Feature img" />
-              <Box
-                sx={{
-                  position: 'absolute',
-                  top: -36,
-                  right: { xs: 0, md: -36 },
-                  boxShadow: 2,
-                  borderRadius: 1,
-                  px: 2.2,
-                  py: 1.4,
-                  zIndex: 1,
-                  backgroundColor: 'background.paper',
-                  width: 190,
-                }}
-              >
-                <Typography variant="h5" sx={{ mb: 1 }}>
-                  Lorem ipsum dolor
-                </Typography>
-                <Box sx={{ mb: 2 }}>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    UI/UI Design
-                  </Typography>
-                  <BorderLinearProgress variant="determinate" color="inherit" value={65} order={1} />
-                </Box>
-                <Box sx={{ mb: 2 }}>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    Mobile Development
-                  </Typography>
-                  <BorderLinearProgress variant="determinate" color="inherit" value={40} order={2} />
-                </Box>
-                <Box sx={{ mb: 2 }}>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    Web Development
-                  </Typography>
-                  <BorderLinearProgress variant="determinate" color="inherit" value={50} order={3} />
-                </Box>
-              </Box>
-
-              <Box
-                sx={{
-                  position: 'absolute',
-                  bottom: -12,
-                  left: { xs: 0, md: -24 },
-                  boxShadow: 2,
-                  borderRadius: 1,
-                  px: 2.2,
-                  py: 2,
-                  zIndex: 1,
-                  backgroundColor: 'background.paper',
-                  textAlign: 'center',
-                }}
-              >
-                <Box
-                  sx={{
-                    position: 'relative',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                  }}
-                >
-                  <Typography sx={{ fontWeight: 600, lineHeight: 1 }}>Lorem ipsum</Typography>
-                  <Typography variant="subtitle1" sx={{ mb: 1, color: 'text.disabled' }}>
-                    Lorem ipsum
-                  </Typography>
-                  <Box
-                    sx={{
-                      height: 85,
-                      width: 85,
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      flexDirection: 'column',
-                    }}
-                  >
-                    <Typography variant="h4" sx={{ color: '#32dc88' }}>
-                      75%
-                    </Typography>
-                    <CircularProgress
-                      sx={{ position: 'absolute', color: 'divider' }}
-                      thickness={2}
-                      variant="determinate"
-                      value={85}
-                      size={85}
-                    />
-                    <CircularProgress
-                      disableShrink
-                      thickness={2}
-                      variant="determinate"
-                      value={75}
-                      size={85}
-                      sx={{ transform: 'rotate(96deg) !important', color: '#32dc88', position: 'absolute' }}
-                    />
-                  </Box>
-                </Box>
-              </Box>
+              <Image src="/images/home-about.svg" width={650} height={678} quality={97} alt="Feature img" />
             </Box>
           </Grid>
           <Grid item xs={12} md={7}>
@@ -149,77 +55,34 @@ const HomeFeature: FC = () => {
                 ml: { xs: 0, md: 4 },
                 mt: 2,
                 mb: 3,
-                lineHeight: 1,
+                lineHeight: 1.8,
                 fontWeight: 'bold',
+                color : 'white',
               }}
             >
-              Make your{' '}
-              <Typography
-                component="mark"
-                sx={{
-                  position: 'relative',
-                  color: 'primary.main',
-                  fontSize: 'inherit',
-                  fontWeight: 'inherit',
-                  backgroundColor: 'unset',
-                }}
-              >
-                Learning <br />
-                <Box
-                  sx={{
-                    position: 'absolute',
-                    top: { xs: 20, md: 28 },
-                    transform: 'rotate(3deg)',
-                    left: 2,
-                    '& img': { width: { xs: 140, md: 175 }, height: 'auto' },
-                  }}
-                >
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src="/images/headline-curve.svg" alt="Headline curve" />
-                </Box>
-              </Typography>
-              Enjoyable
+              About Solicy
             </Typography>
 
-            <Typography sx={{ color: 'text.secondary', mb: 2, ml: { xs: 0, md: 4 } }}>
-              Set the way of learning according to your wishes with some of the benefits that you get us, so you on
-              enjoy the lessons that we provide.
+            <Typography sx={{ color: 'white', fontSize: '24px', mb: 2, ml: { xs: 0, md: 4 } }}>
+              Solicy is a fast-growing company providing Blockchain and Software solutions to customers from around the globe. Due to the high demand for Blockchain development services in the market, we keep up with innovations and provide a wide range of services.
             </Typography>
+            <Button sx={{
+              background: 'white',
+              borderRadius: '10px',
+              width : '200px',
+              height: '50px',
+              boxShadow: '0px 1px gray',
+              ml: { xs: 0, md: 4 },
+              '&:hover' : {
+                background: 'white',
+              },
+              letterSpacing : '3px',
+              fontWeight : 'bold'
+            }}
+              size="large" variant="outlined" >
+              See More
+            </Button>
 
-            <Grid container spacing={2} sx={{ ml: { xs: 0, md: 2 } }}>
-              {data.map(({ title, description, icon }, index) => (
-                <Grid key={String(index)} item xs={12} md={6}>
-                  <Box sx={{ px: 2, py: 1.5, boxShadow: 1, borderRadius: 4, display: 'flex', alignItems: 'center' }}>
-                    <Box
-                      sx={{
-                        mr: 1,
-                        backgroundColor: 'primary.main',
-                        borderRadius: '50%',
-                        height: 36,
-                        width: 36,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        color: 'primary.contrastText',
-                        '& svg': {
-                          fontSize: 20,
-                        },
-                      }}
-                    >
-                      {icon}
-                    </Box>
-                    <Box sx={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
-                      <Typography variant="h6" sx={{ fontSize: '1rem', mb: 1, color: 'secondary.main' }}>
-                        {title}
-                      </Typography>
-                      <Typography sx={{ lineHeight: 1.3, color: 'text.secondary' }} variant="subtitle1">
-                        {description}
-                      </Typography>
-                    </Box>
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
           </Grid>
         </Grid>
       </Container>
